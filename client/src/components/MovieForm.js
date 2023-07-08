eb054c0d7e3f9a0ed0e77d5e47e2e54e793956b6
gich2009 <waneneamos@gmail.com>
2023-07-08 21:37:47 +0300

    Completed assignment

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors, setErrors] = useState([]);
+
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -13,18 +15,26 @@ function MovieForm() {
     discount: false,
     female_director: false,
   });
-
-  function handleSubmit(e) {
+  
+  async function handleSubmit(e) {
     e.preventDefault();
-    fetch("/movies", {
+
+    const response = await fetch("/movies", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+    });
+
+    const data = await response.json();
+
+    if (response.ok){
+      console.log("Movie created:", data);
+    } else{
+      setErrors(data.errors);
+      console.log(errors);
+    }
   }
 
   function handleChange(e) {
